5be24506fa8781c0f2832af8c738b238e12382c7
suhanikundu <126338201+suhanikundu@users.noreply.github.com>
2024-06-06 15:52:50 +0530

    Add files via upload

---
+++ abc.js
@@ -0,0 +1,104 @@
+let boxes = document.querySelectorAll(".boxes");
+let currPlayer="X";
+let message=document.querySelector(".messagebox");
+let rstbtn=document.querySelector("#reset")
+let arr=Array(9).fill(null);
+
+function displaywinner (winner) {
+    message.classList.remove("hide");
+    message.innerText=`Congratulations! Winner is ${winner}`;
+    disablebtn();
+}
+
+
+function  disablebtn(){
+    boxes.forEach(box => {
+        box.disabled = true;
+    });
+}
+
+function checkWinner(){
+    if(
+        (arr[0] !==null && arr[0] === arr[1] && arr[1] === arr[2])||
+        (arr[3] !==null && arr[3] === arr[4] && arr[4] === arr[5])||
+        (arr[6] !==null && arr[6] === arr[7] && arr[7] === arr[8])||
+        (arr[0] !==null && arr[0] === arr[3] && arr[3] === arr[6])||
+        (arr[1] !==null && arr[1] === arr[4] && arr[4] === arr[7])||
+        (arr[2] !==null && arr[2] === arr[5] && arr[5] === arr[8])||
+        (arr[0] !==null && arr[0] === arr[4] && arr[4] === arr[8])||
+        (arr[2] !==null && arr[2] === arr[4] && arr[4] === arr[6])
+    )
+    {
+        displaywinner(currPlayer);
+        
+    }else if (arr.every(item => item !== null)) {
+        // Check for tie
+        message.classList.remove("hide");
+        message.innerText = "It's a tie!";
+    }
+    else if(arr.every(item=>item!==null)){
+        message.classList.remove("hide");
+        message.innerText="It's a tie";
+    }
+    else{
+        message.lea
+    }
+}
+
+
+boxes.forEach(box => {
+    box.addEventListener("click", () => {
+        arr[box.id]=currPlayer;
+        if(arr[box.id]!==null){
+            box.disabled = true;
+        }
+        box.innerText=currPlayer;
+        checkWinner();
+        currPlayer==="X"?currPlayer="O":currPlayer="X";
+        console.log(arr);
+    });
+});
+boxes.forEach()
+rstbtn.addEventListener("click",()=>{
+    currPlayer="X";
+    boxes.forEach(box => {
+        box.innerText="";
+        box.disabled=false;
+    });
+    arr.fill(null);
+    message.classList.add("hide");
+
+})
+rstbtn.addEventListener("click",()=>{
+    currPlayer="X";
+    boxes.forEach(box=>{
+        box.innerText="";
+        box.disabled=false;
+    });
+})
+
+rstbtn.addEventListener("click",()=>{
+    currPlayer="X";
+    boxes.forEach(box=>{
+        box.innerText="";
+        box.disabled=false;
+    });
+});
+rstbtn.addEventListener("click",()=>{
+    currPlayer="X";
+    boxes.forEach(box=>{
+        box.innerHTML="hi hello how are you";
+        box.disabled-6 
+    })
+})
+rstbtn.addEventListener("click",()=>{
+    currPlayer="X";
+    boxes.forEach(box=>{
+        box.innerText="";
+        box.disabled=false;
+    });
+})
+rstbtn.addEventListener("click",()=>{
+    currPlayer="X";
+    boxes.innerText=" "
+})
